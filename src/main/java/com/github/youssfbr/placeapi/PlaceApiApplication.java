98e0f8141114790a282dc5a24e34cc0dfb28d7b5
Alisson Youssf <youssfbr@gmail.com>
2023-06-19 23:14:28 -0300

    Project created

---
+++ src/main/java/com/github/youssfbr/placeapi/PlaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.placeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/youssfbr/placeapi/PlaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.placeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
